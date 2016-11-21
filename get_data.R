c861d149badae4aaa3779d31f54975853cbd55a8
jemus42 <jemus42@quantenbrot.de>
2016-11-21 18:13:19 +0100

    Automatic relay show feed collection

--- get_data.R
+++ get_data.R
@@ -5,7 +5,12 @@
 source("podcast_parsers.R")
 source("setup.R")
 
-relay <- get_relay_shows()
+relay_shows <- read_html("https://www.relay.fm/shows") %>%
+  html_nodes("h4 a") %>%
+  html_attr("href") %>%
+  str_c("https://www.relay.fm", ., "/feed")
+
+relay <- get_relay_shows(relay_shows)
 cache_podcast_data(relay)
 
 ## Incomparable
--- podcast_parsers.R
+++ podcast_parsers.R
@@ -42,38 +42,8 @@ parse_relay_feed <- function(url = "https://www.relay.fm/master/feed"){
   return(df)
 }
 
-get_relay_shows <- function() {
-  relay_urls <-
-    c("https://www.relay.fm/analogue/feed",
-      "https://www.relay.fm/almanac/feed",
-      "https://www.relay.fm/bonanza/feed",
-      "https://www.relay.fm/b-sides/feed",
-      "https://www.relay.fm/canvas/feed",
-      "https://www.relay.fm/clockwise/feed",
-      "https://www.relay.fm/connected/feed",
-      "https://www.relay.fm/cortex/feed",
-      "https://www.relay.fm/disruption/feed",
-      "https://www.relay.fm/freeagents/feed",
-      "https://www.relay.fm/ltoe/feed",
-      "https://www.relay.fm/isometric/feed",
-      "https://www.relay.fm/liftoff/feed",
-      "https://www.relay.fm/mpu/feed",
-      "https://www.relay.fm/material/feed",
-      "https://www.relay.fm/mixedfeelings/feed",
-      "https://www.relay.fm/rd/feed",
-      "https://www.relay.fm/remaster/feed",
-      "https://www.relay.fm/rocket/feed",
-      "https://www.relay.fm/penaddict/feed",
-      "https://www.relay.fm/presentable/feed",
-      "https://www.relay.fm/tc/feed",
-      "https://www.relay.fm/topfour/feed",
-      "https://www.relay.fm/radar/feed",
-      "https://www.relay.fm/upgrade/feed",
-      "https://www.relay.fm/ungeniused/feed",
-      "https://www.relay.fm/inquisitive/feed",
-      "https://www.relay.fm/virtual/feed")
-
-  relay <- plyr::ldply(relay_urls, function(x){
+get_relay_shows <- function(urls) {
+  relay <- plyr::ldply(urls, function(x){
     parse_relay_feed(x)
   })
 
