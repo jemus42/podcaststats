a23e8ba9c3fc4c0b4bf1be14b925b9d46411037a
jemus42 <jemus42@quantenbrot.de>
2016-11-24 01:07:41 +0100

    Finally a good ATP data source

--- podcast_parsers.R
+++ podcast_parsers.R
@@ -51,29 +51,131 @@ get_relay_shows <- function(urls) {
 }
 
 #### ATP ####
+# Thanks, Marco
+# https://twitter.com/marcoarment/status/801550714568900608
 
-parse_atp_feed <- function(url = "http://atp.fm/episodes?format=rss") {
-  atpfeed <- read_html(url)
+parse_atp_feed <- function(url = "https://overcast.fm/itunes617416468/accidental-tech-podcast") {
+  raw    <- read_html(url)
+  titles <- html_nodes(raw, ".title") %>%
+              html_text() %>%
+              str_replace_all("\\t", "")
+  nums   <- as.numeric(str_extract(titles, "^\\d+"))
 
-  titles    <- atpfeed %>% html_nodes("title") %>%
-    html_text() %>%
-    str_replace_all("\\t", "")
+  meta   <- html_nodes(raw, ".caption2.singleline") %>% html_text()
+  pub    <- str_extract(meta, "^\\n\\s+.*\\u2022") %>%
+              str_replace_all("^\\n\\s+", "") %>%
+              str_replace("\\s\\u2022$", "")
+  yr     <- as.numeric(str_extract(pub, "\\d{4}$"))
+  yr     <- ifelse(is.na(yr), as.numeric(format(now(), "%Y")), yr)
 
-  number <- str_extract(titles, "^\\d+")
+  pub    <- str_replace(pub, ",\\s\\d+$", "")
+  pub    <- mdy(paste0(pub, " ", yr))
 
-  durations <- atpfeed %>% html_nodes("duration") %>%
-    html_text() %>%
-    parse_duration()
+  drt    <- str_extract(meta, "\\u2022.*$") %>%
+              str_replace_all("\\u2022\\s", "") %>%
+              str_trim() %>%
+              parse_duration()
 
-  pubdate   <- atpfeed %>% html_nodes("pubdate") %>%
-    html_text() %>%
-    str_replace("^.{5}", "") %>%
-    lubridate::parse_date_time("%d %b %Y %H:%M:%S %z")
+  summ  <- html_nodes(raw, ".lighttext.margintop05") %>% html_text() %>%
+            str_replace_all("^\\n", "") %>%
+            str_trim("both")
 
-  atp <- tibble(number = number[-1], title = titles[-1],
-                duration = durations, date = pubdate) %>%
-            mutate(month = month(date, label = T, abbr = F),
-                   year  = as.factor(year(date)))
-
-  return(atp)
+  tibble(number = nums, title = titles, date = pub, month = month(pub, label = T, abbr = F),
+         year = yr, duration = drt, summary = summ)
 }
+
+# parse_atp_feed <- function(url = "http://atp.fm/episodes?format=rss") {
+#   atpfeed <- read_html(url)
+#
+#   titles    <- atpfeed %>% html_nodes("title") %>%
+#     html_text() %>%
+#     str_replace_all("\\t", "")
+#
+#   number <- str_extract(titles, "^\\d+")
+#
+#   durations <- atpfeed %>% html_nodes("duration") %>%
+#     html_text() %>%
+#     parse_duration()
+#
+#   pubdate   <- atpfeed %>% html_nodes("pubdate") %>%
+#     html_text() %>%
+#     str_replace("^.{5}", "") %>%
+#     lubridate::parse_date_time("%d %b %Y %H:%M:%S %z")
+#
+#   atp <- tibble(number = number[-1], title = titles[-1],
+#                 duration = durations, date = pubdate) %>%
+#             mutate(month = month(date, label = T, abbr = F),
+#                    year  = as.factor(year(date)))
+#
+#   return(atp)
+# }
+#
+# latest_atp_num <- function() {
+#   read_html("http://atp.fm/") %>%
+#     html_nodes(".entry-title a") %>%
+#     html_text() %>%
+#     extract2(1) %>%
+#     str_extract("^\\d+") %>%
+#     as.numeric()
+# }
+#
+# download_atp_episodes <- function() {
+#   plyr::l_ply(seq_len(latest_atp_num()), function(num) {
+#
+#     file <- paste0("data/atp/atp", num, ".mp3")
+#
+#     if (!file.exists(file)) {
+#       url <- paste0("http://traffic.libsyn.com/atpfm/atp", num, ".mp3")
+#       download.file(url, file)
+#     }
+#   })
+# }
+#
+# get_atp_data_from_files <- function(dir = "data/atp/") {
+#   files <- list.files(dir, pattern = "atp\\d+.mp3", full.names = T)
+#
+#   nums      <- as.numeric(str_extract(files, "\\d+"))
+#   durations <- get_mp3_duration(files)
+#   titles    <- get_mp3_title(files)
+#
+#   ret <- tibble(number = nums, title = titles, duration = durations) %>%
+#            arrange(desc(number))
+# }
+#
+# #### Parsing generic mp3 files as a fallback ####
+# get_mp3_duration <- function(file, unit = "min") {
+#
+#   if (length(file) > 1) {
+#     ret <- sapply(file, get_mp3_duration, unit = unit, USE.NAMES = F)
+#     return(ret)
+#   }
+#
+#   cmd <- "mediainfo --Inform='General;%Duration%'"
+#   cmd <- paste(cmd, file)
+#
+#   ret <- as.numeric(system(cmd, intern = T))
+#   ret <- ret/1000
+#
+#   if (unit == "sec") {
+#    return(ret)
+#   } else if (unit == "min") {
+#     ret <- ret/60
+#     return(ret)
+#   } else {
+#     stop("Invalid unit specification")
+#   }
+# }
+#
+# get_mp3_title <- function(file) {
+#
+#   if (length(file) > 1) {
+#     ret <- sapply(file, get_mp3_title, USE.NAMES = F)
+#     return(ret)
+#   }
+#
+#   cmd <- "mediainfo --Inform='General;%Title%'"
+#   cmd <- paste(cmd, file)
+#
+#   ret <- system(cmd, intern = T)
+#   return(ret)
+# }
