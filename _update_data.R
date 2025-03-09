6004a075f17987116e705c13d1c10e25b91b1de9
Lukas Burk <lukas@quantenbrot.de>
2025-03-09 14:41:51 +0100

    Formatting

--- _update_data.R
+++ _update_data.R
@@ -7,24 +7,24 @@ cli::cli_ul()
 
 cli::cli_li("Getting ATP")
 
-  atp <- atp_get_episodes()
-  cache_podcast_data(atp, csv = FALSE)
+atp <- atp_get_episodes()
+cache_podcast_data(atp, csv = FALSE)
 
 cli::cli_li("Getting relay.fm")
 
-  relay_shows <- relay_get_shows()
-  relay_episodes <- relay_get_episodes(relay_shows)
+relay_shows <- relay_get_shows()
+relay_episodes <- relay_get_episodes(relay_shows)
 
-  cache_podcast_data(relay_shows)
-  cache_podcast_data(relay_episodes)
+cache_podcast_data(relay_shows)
+cache_podcast_data(relay_episodes)
 
 cli::cli_li("Getting The Incomparable")
 
-  incomparable_shows <- incomparable_get_shows()
-  incomparable_episodes <- incomparable_get_episodes(incomparable_shows)
+incomparable_shows <- incomparable_get_shows()
+incomparable_episodes <- incomparable_get_episodes(incomparable_shows)
 
-  cache_podcast_data(incomparable_shows)
-  cache_podcast_data(incomparable_episodes)
+cache_podcast_data(incomparable_shows)
+cache_podcast_data(incomparable_episodes)
 
 cli::cli_end()
 
