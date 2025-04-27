3c803b88eb62f00c651c5253b28817e6b14da6b5
Lukas Burk <lukas@quantenbrot.de>
2025-04-27 16:03:14 +0200

    bump

--- _update_data.R
+++ _update_data.R
@@ -8,15 +8,11 @@ cli::cli_ul()
 cli::cli_li("Getting ATP")
 
 atp <- atp_get_episodes()
-cache_podcast_data(atp, csv = FALSE)
 
 cli::cli_li("Getting relay.fm")
 
 relay_shows <- relay_get_shows()
 relay_episodes <- relay_get_episodes(relay_shows)
-
-cache_podcast_data(relay_shows)
-cache_podcast_data(relay_episodes)
 
 cli::cli_li("Getting The Incomparable")
 
